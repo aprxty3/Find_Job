1e9d577110ff0c6fa539726e6ec1d01f359c938f
aprxrt3 <ajiprxty3@gmail.com>
2021-11-21 13:04:10 +0700

    change parameter name

--- lib/gender_provider.dart
+++ lib/gender_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class genderProvider with ChangeNotifier {
+class GenderProvider with ChangeNotifier {
   bool _isMale = true;
 
   bool get isMale => _isMale;
