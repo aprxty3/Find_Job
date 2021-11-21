0d397286c91e37a1e03a216fb4364b563ec24519
aprxrt3 <ajiprxty3@gmail.com>
2021-11-21 10:53:46 +0700

    bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
+        
         body: Center(
           child: Container(
             child: Text('Hello World'),
